71f8a8010727bc73fd8e6580913eeebd2a4885df
Choi-UngGyu <cdr982116@gmail.com>
2020-11-27 11:20:41 +0900

    2.1 Login From part One

---
+++ src/routes/Auth.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <span>
+            <from onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange} />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required value={password}
+                    onChange={onChange} />
+                <input type="submit" value="Log In" />
+                <div>
+                    <button>Continue with Google</button>
+                    <button>Continue with Github</button>
+                </div>
+            </from>
+        </span>
+    )
+}
+
+export default Auth;
