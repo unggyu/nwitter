368c370b039c3e7f65b48b6b3e189621fc88b2fa
unggyu <cdr982116@gmail.com>
2020-11-29 14:34:19 +0900

    4.2 Uploading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Nweet from 'components/Nweet';
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
 
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
@@ -17,12 +18,15 @@ const Home = ({ userObj }) => {
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
-        await dbService.collection("nweets").add({
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid
-        });
-        setNweet("");
+        const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+        const response = await fileRef.putString(attachment, 'data_url');
+        console.log(response);
+        // await dbService.collection("nweets").add({
+        //     text: nweet,
+        //     createdAt: Date.now(),
+        //     creatorId: userObj.uid
+        // });
+        // setNweet("");
     };
     const onChange = (event) => {
         const {
